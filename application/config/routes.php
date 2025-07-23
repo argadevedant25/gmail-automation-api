70837a1c264058f0b7ee765b45a06d96a963e712
VEDANT SANJAY ARGADE <86397216+argadevedant25@users.noreply.github.com>
2025-07-23 11:27:33 +0530

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,4 +50,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'TicketController/create';
-$route['ticketcontroller/create'] = 'TicketController/create';
+$route['api/ticket/create'] = 'TicketController/create';
