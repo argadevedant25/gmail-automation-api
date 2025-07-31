10d3b0baed2ebecb1847a24110d72003b9c86e0b
VEDANT SANJAY ARGADE <86397216+argadevedant25@users.noreply.github.com>
2025-07-31 14:30:00 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -6,10 +6,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
     'dsn' => getenv('DATABASE_URL') ?: 'pgsql:host=ep-noisy-bonus-a1n85050.ap-southeast-1.aws.neon.tech;port=5432;dbname=ticket_system;user=neondb_owner;password=npg_U6g5sCwXIPoZ;sslmode=require;options=endpoint=ep-noisy-bonus-a1n85050',
-    'hostname' => 'ep-noisy-bonus-a1n85050.ap-southeast-1.aws.neon.tech',
-    'username' => 'neondb_owner',
-    'password' => 'npg_U6g5sCwXIPoZ',
-    'database' => 'ticket_system',
+    'hostname' => '',
+    'username' => '',
+    'password' => '',
+    'database' => '',
     'dbdriver' => 'postgre',
     'pconnect' => FALSE,
     'db_debug' => FALSE, // Disable to prevent header errors
@@ -18,14 +18,12 @@ $db['default'] = array(
     'char_set' => 'utf8',
     'dbcollat' => 'utf8_general_ci',
     'swap_pre' => '',
-    'encrypt' => array('sslmode' => 'require', 'options' => 'endpoint=ep-noisy-bonus-a1n85050'),
+    'encrypt' => TRUE, // Enable SSL
     'compress' => FALSE,
     'stricton' => FALSE,
     'failover' => array(),
-    'save_queries' => TRUE,
-    'port' => 5432 // Explicit port
+    'save_queries' => TRUE
 );
-
 // Debug log for DATABASE_URL
 log_message('debug', 'DATABASE_URL: ' . getenv('DATABASE_URL'));
 ?>
